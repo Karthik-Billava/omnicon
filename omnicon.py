078d2c0cd2d8c3bf419f8a43bbc633249e264578
Karthik-Billava <karthikbillava1107@gmail.com>
2024-08-10 16:13:58 +0530

    Add multiple exception blocks for improved error handling

--- omnicon.py
+++ omnicon.py
@@ -1,12 +1,13 @@
 import argparse
+from pdf2docx import Converter
 import docx2pdf
 import sys
 import os
 
-def path_nn(fn,newname,typ):
-    path_split=fn.split("\\")
-    only_old=path_split[-1]
-    nn=fn.replace(only_old,f"{newname}.{typ}")
+def path_nn(fn, newname, typ):
+    path_split = fn.split("\\")
+    only_old = path_split[-1]
+    nn = fn.replace(only_old, f"{newname}.{typ}")
     return nn
 
 def main():
@@ -28,20 +29,41 @@ def main():
             case "pdf":
                 if fn.endswith(".docx"):
                     if args.newname:
-                        nn = path_nn(fn,args.newname,args.type)
-                        docx2pdf.convert(fn,nn) 
+                        nn = path_nn(fn, args.newname, args.type)
+                        docx2pdf.convert(fn, nn)
                     else:
                         docx2pdf.convert(fn, fn.replace("docx", "pdf"))
+            case "docx":
+                if fn.endswith(".pdf"):
+                    if args.newname:
+                        nn = path_nn(fn, args.newname, args.type)
+                    else:
+                        nn = fn.replace("pdf", "docx")
+                    cv = Converter(fn)
+                    cv.convert(nn, start=0, end=None)
+                    cv.close()
+
+    except NotADirectoryError:
+        print("File not found error")
+    except IsADirectoryError:
+        print("File not found error")
+    except FileNotFoundError:
+        print("File not found, please check the file path and try again.")
+    except PermissionError:
+        print("Permission denied, please check the file path and try again.")
+    except OSError:
+        print("OS error, please check the file path and try again.")
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt Error")
     except Exception as e:
         print(e)
-
+        
     print(args.file_name)
     print(args.type)
     if args.newname:
         print(args.newname)
     else:
         print("No new name provided")
-
 
 if __name__ == "__main__":
     main()

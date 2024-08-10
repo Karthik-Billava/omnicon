d7ab82723de9b5ba5afdaa41730f47b277dbd8a1
Karthik-Billava <karthikbillava1107@gmail.com>
2024-08-10 16:57:48 +0530

    Refactor to eliminate hard-coded values by implementing a dynamic solution

--- omnicon.py
+++ omnicon.py
@@ -1,14 +1,18 @@
 import argparse
+import mdpdf.converter
 from pdf2docx import Converter
 import docx2pdf
+import mdpdf
 import sys
 import os
+
 
 def path_nn(fn, newname, typ):
     path_split = fn.split("\\")
     only_old = path_split[-1]
     nn = fn.replace(only_old, f"{newname}.{typ}")
     return nn
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -21,24 +25,22 @@ def main():
         help="The convertion to perform: Convertion to pdf, docx, ppt or md")
     parser.add_argument("-n", "--newname", type=str,
                         help="New name of the file sy: --newname [new_name]")
-    args = parser.parse_args()
 
+    args = parser.parse_args()
     fn = args.file_name
+    if args.newname:
+        nn = path_nn(fn, args.newname, args.type)
+    else:
+        nn = fn.replace(fn.split(".")[-1], args.type)
+
     try:
         match args.type:  # pdf to docx conversion
             case "pdf":
                 if fn.endswith(".docx"):
-                    if args.newname:
-                        nn = path_nn(fn, args.newname, args.type)
-                        docx2pdf.convert(fn, nn)
-                    else:
-                        docx2pdf.convert(fn, fn.replace("docx", "pdf"))
+                    docx2pdf.convert(fn, nn)
+
             case "docx":
                 if fn.endswith(".pdf"):
-                    if args.newname:
-                        nn = path_nn(fn, args.newname, args.type)
-                    else:
-                        nn = fn.replace("pdf", "docx")
                     cv = Converter(fn)
                     cv.convert(nn, start=0, end=None)
                     cv.close()
@@ -57,13 +59,14 @@ def main():
         print("Keyboard Interrupt Error")
     except Exception as e:
         print(e)
-        
+
     print(args.file_name)
     print(args.type)
     if args.newname:
         print(args.newname)
     else:
         print("No new name provided")
+
 
 if __name__ == "__main__":
     main()
